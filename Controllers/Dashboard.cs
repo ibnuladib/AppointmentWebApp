404e00759dca6475fdaf098146b1b9bf54a4db54
Ibnul Adib <adib.bdhk@gmail.com>
2024-09-21 16:48:09 +0600

    Fixed Registration

---
+++ Controllers/Dashboard.cs
@@ -0,0 +1,22 @@
+﻿using AppointmentWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentWebApp.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace AppointmentWebApp.Controllers
 
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
             return View();
         }
 
