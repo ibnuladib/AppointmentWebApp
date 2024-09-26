b88e3fe15cac8c55cfb3dea3ebf176e6e287e993
Ibnul Adib <adib.bdhk@gmail.com>
2024-09-26 15:33:52 +0600

    Create logScript.js

---
+++ wwwroot/js/logScript.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/logHub")
+        .build();
+
+    connection.on("ReceiveLogUpdate", function (message) {
+        const logElement = document.createElement("li");
+        const timestamp = new Date().toLocaleString();
+
+        let combinedString = timestamp.concat(": ", message);
+        logElement.innerHTML = `<span class="timestamp">${timestamp}</span>: ${message}`;
+        document.getElementById("logList").appendChild(logElement);
+    });
+
+    connection.start().catch(function (err) {
+        console.error(err.toString());
+    });
+});
