ea5263bac626bfd657b3fb669e912b2e1256547d
lut-ful <lutfulkabir1757@gmail.com>
2024-09-29 19:24:31 +0600

    dash up 2

---
+++ Views/Dashboard/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppointmentWebApp.Views.Dashboard
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
